f76af7246fb9a60ffe8ad631b5a7cfb2e1e238f9
Farouq Abdurrahman <f.abdurrahman@proventa.de>
2023-08-15 10:45:33 +0200

    no need to recreate cert if existed

--- gen-root-ca.sh
+++ gen-root-ca.sh
@@ -1,3 +1,9 @@
+# If etcd-root-ca.pem and etcd-root-ca-key.pem already exist, then skip this step
+if [ -f certs/etcd-root-ca.pem ] && [ -f certs/etcd-root-ca-key.pem ]; then
+  echo "Root CA already exists. Skipping root CA generation."
+  exit 0
+fi
+
 # Make sure bin and certs directories exist and clean
 mkdir -p bin && rm -f bin/cfssl* && rm -rf certs && mkdir -p certs
 
