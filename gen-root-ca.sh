84b0a828f9ed01fab76866c29d82148a10f8bc05
Farouq Abdurrahman <f.abdurrahman@proventa.de>
2023-08-24 09:53:22 +0200

    update generate root ca script

--- gen-root-ca.sh
+++ gen-root-ca.sh
@@ -6,8 +6,19 @@ if [ -f certs/proventa-etcd-root-ca.pem ] && [ -f certs/proventa-etcd-root-ca-ke
   exit 0
 fi
 
+# Make sure bin and certs directories exist and clean
+mkdir -p bin && rm -f bin/cfssl* && rm -rf certs && mkdir -p certs
+
 # Make sure certs directory exist and clean
 rm -rf certs && mkdir -p certs
+
+# Download cfssl binaries
+curl -L https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 -o bin/cfssl
+chmod +x bin/cfssl
+
+# Download cfssljson binaries
+curl -L https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 -o bin/cfssljson
+chmod +x bin/cfssljson
 
 # Generate root CA certificate bundle (Public Certificate and Private Key)
 cat > certs/proventa-etcd-root-ca-csr.json <<EOF
@@ -28,13 +39,20 @@ cat > certs/proventa-etcd-root-ca-csr.json <<EOF
   "CN": "proventa-etcd-root-ca"
 }
 EOF
+./bin/cfssl gencert --initca=true certs/proventa-etcd-root-ca-csr.json | ./bin/cfssljson --bare certs/proventa-etcd-root-ca
 
-IMAGES=$(podman images | grep cfssl | awk '{print $1}')
-
-# If cfssl imagae is not present, then pull it
-if [ -z "$IMAGES" ]; then
-  echo "Pulling cfssl image"
-  podman pull cfssl/cfssl
-fi
-
-podman run --rm -v ./certs/proventa-etcd-root-ca-csr.json:/proventa-etcd-root-ca-csr.json cfssl/cfssl gencert -initca /proventa-etcd-root-ca-csr.json |  cfssljson --bare ./certs/proventa-etcd-root-ca
+cat > certs/proventa-etcd-gencert-config.json <<EOF
+{
+  "signing": {
+    "default": {
+        "usages": [
+          "signing",
+          "key encipherment",
+          "server auth",
+          "client auth"
+        ],
+        "expiry": "87600h"
+    }
+  }
+}
+EOF
