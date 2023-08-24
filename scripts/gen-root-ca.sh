d30d2b2b8268c83189e4b5d783176f865dea1564
Farouq Abdurrahman <f.abdurrahman@proventa.de>
2023-08-24 16:40:09 +0200

    update shell scripts and yaml files due to changed folder

--- scripts/gen-root-ca.sh
+++ scripts/gen-root-ca.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 # If proventa-etcd-root-ca.pem and proventa-etcd-root-ca-key.pem already exist, then skip this step
-if [ -f certs/proventa-etcd-root-ca.pem ] && [ -f certs/proventa-etcd-root-ca-key.pem ]; then
+if [ -f ../certs/proventa-etcd-root-ca.pem ] && [ -f ../certs/proventa-etcd-root-ca-key.pem ]; then
   echo "Root CA already exists. Skipping root CA generation."
   exit 0
 fi
 
 # Make sure bin and certs directories exist and clean
-mkdir -p bin && rm -f bin/cfssl* && rm -rf certs && mkdir -p certs
+mkdir -p ../bin && rm -f ../bin/cfssl* && rm -rf ../certs && mkdir -p ../certs
 
-# Make sure certs directory exist and clean
-rm -rf certs && mkdir -p certs
 
 # Download cfssl binaries
-curl -L https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 -o bin/cfssl
-chmod +x bin/cfssl
+curl -L https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 -o ../bin/cfssl
+chmod +x ../bin/cfssl
 
 # Download cfssljson binaries
-curl -L https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 -o bin/cfssljson
-chmod +x bin/cfssljson
+curl -L https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 -o ../bin/cfssljson
+chmod +x ../bin/cfssljson
 
 # Generate root CA certificate bundle (Public Certificate and Private Key)
-cat > certs/proventa-etcd-root-ca-csr.json <<EOF
+cat > ../certs/proventa-etcd-root-ca-csr.json <<EOF
 {
   "key": {
     "algo": "rsa",
@@ -39,9 +37,10 @@ cat > certs/proventa-etcd-root-ca-csr.json <<EOF
   "CN": "proventa-etcd-root-ca"
 }
 EOF
-./bin/cfssl gencert --initca=true certs/proventa-etcd-root-ca-csr.json | ./bin/cfssljson --bare certs/proventa-etcd-root-ca
 
-cat > certs/proventa-etcd-gencert-config.json <<EOF
+./../bin/cfssl gencert --initca=true ../certs/proventa-etcd-root-ca-csr.json | ./../bin/cfssljson --bare ../certs/proventa-etcd-root-ca
+
+cat > ../certs/proventa-etcd-gencert-config.json <<EOF
 {
   "signing": {
     "default": {
