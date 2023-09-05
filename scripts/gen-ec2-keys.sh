b11f960ec943f363aa5f220b0e454cd2a82410ee
Farouq Abdurrahman <f.abdurrahman@proventa.de>
2023-09-05 16:08:13 +0200

    monitoring cluster init commit

--- scripts/gen-ec2-keys.sh
+++ scripts/gen-ec2-keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # If core and core.pub and etcd-user and etcd-user.pub exist, then skip this step
-if [ -f ../keys/core ] && [ -f ../keys/core.pub ] && [ -f ../keys/etcd-user ] && [ -f ../keys/etcd-user.pub ] && [ -f ../keys/patroni-user ] && [ -f ../keys/patroni-user.pub ]; then
+if [ -f ../keys/core ] && [ -f ../keys/core.pub ] && [ -f ../keys/etcd-user ] && [ -f ../keys/etcd-user.pub ] && [ -f ../keys/patroni-user ] && [ -f ../keys/patroni-user.pub ] && [ -f ../keys/monitoring-user ] && [ -f ../keys/monitoring-user.pub ]; then
   echo "Keys already exist. Skipping this step."
   exit 0
 fi
@@ -19,3 +19,6 @@ ssh-keygen -t rsa -b 4096 -f ../keys/etcd-user -N '' -C etcd-user
 
 # Generate the patroni-user key
 ssh-keygen -t rsa -b 4096 -f ../keys/patroni-user -N '' -C patroni-user
+
+# Generate the monitoring-user key
+ssh-keygen -t rsa -b 4096 -f ../keys/monitoring-user -N '' -C monitoring-user
