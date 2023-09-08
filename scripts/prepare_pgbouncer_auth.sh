d1da4f5b7238b5885deec10b32e68719b3b9b7b3
Farouq Abdurrahman <f.abdurrahman@proventa.de>
2023-09-08 16:56:43 +0200

    update function to allow superuser to be used

--- scripts/prepare_pgbouncer_auth.sh
+++ scripts/prepare_pgbouncer_auth.sh
@@ -13,7 +13,7 @@ podman exec -it $1 psql -U $2 -d $3 -c "REVOKE ALL PRIVILEGES ON SCHEMA public F
 podman exec -it $1 psql -U $2 -d $3 -c "CREATE SCHEMA IF NOT EXISTS pgbouncer;"
 podman exec -it $1 psql -U $2 -d $3 -c "REVOKE ALL PRIVILEGES ON SCHEMA pgbouncer FROM pgbouncer;"
 podman exec -it $1 psql -U $2 -d $3 -c "GRANT USAGE ON SCHEMA pgbouncer TO pgbouncer;"
-podman exec -it $1 psql -U $2 -d $3 -c "CREATE OR REPLACE FUNCTION pgbouncer.get_auth(username TEXT) RETURNS TABLE(username TEXT, password TEXT) AS \$\$ SELECT rolname::TEXT, rolpassword::TEXT FROM pg_authid WHERE NOT pg_authid.rolsuper AND NOT pg_authid.rolreplication AND pg_authid.rolcanlogin AND pg_authid.rolname <> 'pgbouncer' AND (pg_authid.rolvaliduntil IS NULL OR pg_authid.rolvaliduntil >= CURRENT_TIMESTAMP) AND pg_authid.rolname = \$1; \$\$ LANGUAGE SQL STABLE SECURITY DEFINER;"
+podman exec -it $1 psql -U $2 -d $3 -c "CREATE OR REPLACE FUNCTION pgbouncer.get_auth(username TEXT) RETURNS TABLE(username TEXT, password TEXT) AS \$\$ SELECT rolname::TEXT, rolpassword::TEXT FROM pg_authid WHERE pg_authid.rolcanlogin AND pg_authid.rolname <> 'pgbouncer' AND (pg_authid.rolvaliduntil IS NULL OR pg_authid.rolvaliduntil >= CURRENT_TIMESTAMP) AND pg_authid.rolname = \$1; \$\$ LANGUAGE SQL STABLE SECURITY DEFINER;"
 podman exec -it $1 psql -U $2 -d $3 -c "REVOKE ALL ON FUNCTION pgbouncer.get_auth(username TEXT) FROM PUBLIC, pgbouncer;"
 podman exec -it $1 psql -U $2 -d $3 -c "GRANT EXECUTE ON FUNCTION pgbouncer.get_auth(username TEXT) TO pgbouncer;"
 
