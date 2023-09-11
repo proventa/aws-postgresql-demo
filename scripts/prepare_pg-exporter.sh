7396ee67f1b4eb4d6b5de857b1cfd1948907aa83
Farouq Abdurrahman <f.abdurrahman@proventa.de>
2023-09-11 12:44:45 +0200

    use https for pg exporter

---
+++ scripts/prepare_pg-exporter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat > /usr/local/share/web-config.yml <<EOF
+tls_server_config:
+    cert_file: /etc/ssl/etcd-certs/proventa-etcd-client-cert.pem
+    key_file: /etc/ssl/etcd-certs/proventa-etcd-client-cert-key.pem
+    client_auth_type: "RequireAndVerifyClientCert"
+    client_ca_file: /etc/ssl/etcd-certs/proventa-etcd-root-ca.pem
+EOF
+
+chmod 644 /usr/local/share/web-config.yml
+
+cat > /usr/local/share/postgres_exporter.yml <<EOF
+auth_modules:
+  superuser:
+    type: userpass
+    userpass:
+      username: postgres
+      password: zalando
+    options:
+      sslmode: disable
+EOF
+
+chmod 644 /usr/local/share/postgres_exporter.yml
